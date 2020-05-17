ce7397b7d88f1dd57861147b48d9db9d18dd1e56
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-05-17 18:53:36 +0500

    Fraction
    Homework (not 100%, in this program has some errors in math calculation and not tested to all tests)

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        private double main;
+        private double numerator;
+        private double denumerator;
+        private string[] fileSplit;
+        private string[] arrayToFoundOperations;
+        public Fraction(string fileSplit, string originalString)
+        {
+            FileSplit = fileSplit.Split('+', '-', '*', '/');
+            ArrayToFoundOperations = originalString.Split();
+        }
+        public string[] FileSplit
+        {
+            get { return fileSplit; }
+            set { fileSplit = value; }
+        }
+        public double Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public double Denumerator
+        {
+            get { return denumerator; }
+            set { denumerator = value; }
+        }
+        public double Main
+        {
+            get { return main; }
+            set { main = value; }
+        }
+        public string[] ArrayToFoundOperations
+        {
+            get { return arrayToFoundOperations; }
+            set { arrayToFoundOperations = value; }
+        }
+        private static bool first = true;
+        public string AddInFraction()
+        {
+            for (int i=0; i<FileSplit.Count()-1;i++)
+            {
+                var splitedFirstPartFile = FileSplit[i].Split(' ', '\\').Where(s => s != String.Empty).ToArray<string>();
+                var splitedNextPartFile = FileSplit[i+1].Split(' ','\\').Where(s => s != String.Empty).ToArray<string>();
+                double firstDenumerator = double.Parse(splitedFirstPartFile[2]);
+                double adv = firstDenumerator;
+                double nextDenumerator = double.Parse(splitedNextPartFile[2]);
+                if (splitedFirstPartFile[2] != splitedNextPartFile[2])
+                {
+                    splitedFirstPartFile[1] = (double.Parse(splitedFirstPartFile[1]) * nextDenumerator).ToString();
+                    splitedNextPartFile[1] = (double.Parse(splitedNextPartFile[1]) * firstDenumerator).ToString();
+                    firstDenumerator *= nextDenumerator;
+                    nextDenumerator *= adv;
+                }
+                var operation = FoundOperation(null);
+                switch(operation)
+                {
+                    case "+":
+                        if (first == true)
+                        {
+                            Main += double.Parse(splitedFirstPartFile[0]) + double.Parse(splitedNextPartFile[0]);
+                            Numerator += double.Parse(splitedFirstPartFile[1]) + double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                            first = false;
+                        }
+                        else
+                        {
+                            Main += double.Parse(splitedNextPartFile[0]);
+                            Numerator += double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                        }
+                        break;
+                    case "-":
+                        if (first == true)
+                        {
+                            Main += double.Parse(splitedFirstPartFile[0]) - double.Parse(splitedNextPartFile[0]);
+                            Numerator += double.Parse(splitedFirstPartFile[1]) - double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                            first = false;
+                        }
+                        else
+                        {
+                            Main -= double.Parse(splitedNextPartFile[0]);
+                            Numerator -= double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                        }
+                        break;
+                    case "*":
+                        if (first == true)
+                        {
+                            Main += double.Parse(splitedFirstPartFile[0]) * double.Parse(splitedNextPartFile[0]);
+                            Numerator += double.Parse(splitedFirstPartFile[1]) * double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                            first = false;
+                        }
+                        else
+                        {
+                            Main *= double.Parse(splitedNextPartFile[0]);
+                            Numerator *= double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                        }
+                        break;
+                    case "/":
+                        if (first == true)
+                        {
+                            Main += double.Parse(splitedFirstPartFile[0]) / double.Parse(splitedNextPartFile[0]);
+                            Numerator += double.Parse(splitedFirstPartFile[1]) / double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                            first = false;
+                        }
+                        else
+                        {
+                            Main /= double.Parse(splitedNextPartFile[0]);
+                            Numerator /= double.Parse(splitedNextPartFile[1]);
+                            Denumerator = nextDenumerator;
+                        }
+                        break;
+                    default: break;
+                }
+            }
+            return "Ответ без перевода: "+Main.ToString()+ ' ' +Numerator.ToString() + '/' + Denumerator.ToString();
+        }
+
+        private static int index=0;
+
+        public string FoundOperation(string op)
+        {
+            while (op==null)
+            {
+                switch (arrayToFoundOperations[index])
+                {
+                    case "+":
+                        op = "+";
+                        break;
+                    case "-":
+                        op = "-";
+                        break;
+                    case "*":
+                        op = "*";
+                        break;
+                    case "/":
+                        op = "/";
+                        break;
+                }
+                index++;
+            }
+            return op;
+        }
+
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"0 3\5 - 1 1\2 + 1 3\5 + 1 1\2";
+            Console.WriteLine(file);
+            Console.WriteLine();
+            var frac = new Fraction(file, file);
+            var res = frac.AddInFraction();
+            Console.WriteLine(res);
+
+
+        }
+    }
+}
